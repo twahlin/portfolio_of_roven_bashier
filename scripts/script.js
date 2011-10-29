f8f0f5ad9b4fa2e23641e7c5429ad9980bfb4a5f
Tom Wahlin <tom.wahlin@gmail.com>
2011-10-29 10:03:26 -0500

    Edits for touch class devices

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,15 @@ $(document).ready(function() {
     $('.post_content').addClass('hidden');   
     $('.post_content .hide_content').removeClass('hidden');    
     
+<<<<<<< HEAD
+=======
+    //when close button is clicked, scroll to top    
+    $('.hide_content').click(function() {
+        $('html, body').animate({
+            scrollTop: $('header').offset().top
+        }, 1000);
+    });
+>>>>>>> Edits for touch class devices
     
     //close button closes drawer
     $('.hide_content').click(function() {
@@ -21,6 +30,7 @@ $(document).ready(function() {
         
         });
     });    
+<<<<<<< HEAD
     
     //toggle active class and open project drawer    
     $('.post_head').click(function() {
@@ -42,7 +52,42 @@ $(document).ready(function() {
     //   
     // $('.post').each(function(index) {
     //     $(this).delay(100*index).fadeIn(1000);
+=======
+    // 
+    // 
+    // //toggle active class and open project drawer    
+    // $('.post_head').click(function() {
+    //  $(this).next().slideToggle('slow', function() {
+    // 
+    //     });
+    //     $(this).toggleClass('post_head_active');
+>>>>>>> Edits for touch class devices
     // });
+    //   
+    // //better to animate opacity here item fade in
+    // // $('.post').css("display", "none");
+    // //   
+    // // $('.post').each(function(index) {
+    // //     $(this).delay(100*index).fadeIn(1000);
+    // // });
+    
+
+	$('.post_head').click(function() {
+		if($(this).hasClass('post_head_active')) {
+			$(this).next('.post_content').slideUp('slow',function() {
+				$(this).prev().removeClass('post_head_active');
+			});
+		} else {
+			$('.post_content').slideUp('slow',function() {
+				$(this).prev().removeClass('post_head_active');
+			});
+			$(this).next('.post_content').slideToggle('slow',function() {
+				$(this).prev().toggleClass('post_head_active');
+			});
+		}
+		return false;
+	});
+
       
     
 });
